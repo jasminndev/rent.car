2be49482aa49d486e01dcdbd11b236f4392f18d7
itsjasminn <muvvahhida@gmail.com>
2025-08-05 16:34:41 +0500

    updated

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 from authentication.models import User
 from authentication.serializers import UserModelSerializer, UserUpdateSerializer, ChangePasswordSerializer
@@ -40,3 +41,10 @@ class ChangePasswordAPIView(UpdateAPIView):
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = ChangePasswordSerializer
+
+
+@extend_schema(tags=['auth'])
+class UserDeleteAPIView(DestroyAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserModelSerializer
