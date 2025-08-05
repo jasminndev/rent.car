1cff736791e57bee0d6f0f47d8623aef2648e4e1
itsjasminn <muvvahhida@gmail.com>
2025-08-05 16:15:29 +0500

    updated

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,42 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from authentication.models import User
-from authentication.serializers import UserModelSerializer
+from authentication.serializers import UserModelSerializer, UserUpdateSerializer, ChangePasswordSerializer
 
 
 @extend_schema(tags=['auth'])
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+
+
+@extend_schema(tags=['auth'])
+class UserUpdateAPIView(UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserUpdateSerializer
+    lookup_field = 'pk'
+
+
+@extend_schema(tags=['auth'])
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = UserModelSerializer
+
+
+@extend_schema(tags=['auth'])
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserModelSerializer
+    lookup_field = 'pk'
+
+
+@extend_schema(tags=['passwd'])
+class ChangePasswordAPIView(UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = ChangePasswordSerializer
