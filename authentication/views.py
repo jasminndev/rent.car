4edd231c4916d416b74fdae59a4f15e36af428c7
itsjasminn <muvvahhida@gmail.com>
2025-08-29 15:53:04 +0500

    updated

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,72 @@
+import random
+from http import HTTPStatus
+
+from django.utils import timezone
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView
+from orjson import orjson
+from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView, \
+    GenericAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from authentication.models import User, Wishlist
 from authentication.serializers import UserModelSerializer, UserUpdateSerializer, ChangePasswordSerializer, \
-    WishlistModelSerializer
+    WishlistModelSerializer, VerifyCodeSerializer
+from authentication.tasks import send_code_email
+from root.settings import redis
 
 
 ###################################### AUTH ######################################
+@extend_schema(tags=['auth'])
+class UserGenericAPIView(GenericAPIView):
+    serializer_class = UserModelSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        code = str(random.randrange(10 ** 5, 10 ** 6))
+        send_code_email.delay(user, code)
+        redis.set(code, orjson.dumps(user))
+        return Response({'message': 'Tastiqlash kodi jonatilid'}, status=HTTPStatus.OK)
+
+
+@extend_schema(tags=['auth'])
+class VerifyCodeGenericAPIView(GenericAPIView):
+    serializer_class = VerifyCodeSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user_data = serializer.context.get('user_data')
+        user = User.objects.create(**user_data)
+        return Response(UserModelSerializer(user).data, status=HTTPStatus.CREATED)
+
+
+@extend_schema(tags=['auth'])
+class CustomTokenObtainPairView(TokenObtainPairView):
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == HTTPStatus.OK:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            user = serializer.user
+            user.last_login = timezone.now()
+            user.save(update_fields=['last_login'])
+        return response
+
+
+@extend_schema(tags=['auth'])
+class CustomTokenRefreshView(TokenRefreshView):
+    pass
+
+
+###################################### USER ######################################
 @extend_schema(tags=['auth'])
 class UserCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
@@ -15,14 +74,14 @@ class UserCreateAPIView(CreateAPIView):
     permission_classes = [AllowAny]
 
 
-@extend_schema(tags=['auth'])
+@extend_schema(tags=['user'])
 class UserUpdateAPIView(UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserUpdateSerializer
     lookup_field = 'pk'
 
 
-@extend_schema(tags=['auth'])
+@extend_schema(tags=['user'])
 class UserDeleteAPIView(DestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
